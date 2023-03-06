d37ed8d530aa091722ed3a145b95686232394bdf
7Matteo <matteo.fiore@student.vinci.be>
2023-03-06 14:13:23 +0100

    add graph class and methods

---
+++ Graph.java
@@ -0,0 +1,18 @@
+import java.io.File;
+
+public class Graph {
+  private File lignes;
+  private File troncons;
+
+  public Graph(File lignes, File troncons) {
+    this.lignes = lignes;
+    this.troncons = troncons;
+  }
+
+
+  public void calculerCheminMinimisantNombreTroncons(String boileau, String alma) {
+  }
+
+  public void calculerCheminMinimisantTempsTransport(String boileau, String alma) {
+  }
+}
