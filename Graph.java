2d8b30829a3664e91281bf02e274da8e0ffde85f
7Matteo <matteo.fiore@student.vinci.be>
2023-03-13 11:31:13 +0100

    fazefxezfz

--- Graph.java
+++ Graph.java
@@ -13,7 +13,7 @@ import java.util.Set;
 public class Graph {
 
   private Set<Station> stationsSet;
-  private Map<Station,Station> mapTroncons;
+  private Map<Station,Set<Station>> mapTroncons;
 
   public Graph(File lignes, File troncons) throws IOException {
     FileReader frLignes = new FileReader(lignes);
@@ -31,17 +31,38 @@ public class Graph {
       Station arrivee = new Station(tabTroncons[2]);
       stationsSet.add(depart);
       stationsSet.add(arrivee);
-      Troncon troncon = new Troncon(Integer.parseInt(tabTroncons[0]), depart, arrivee, Integer.parseInt(tabTroncons[3]));
-      mapTroncons.put(arrivee,depart);
+      //Troncon troncon = new Troncon(Integer.parseInt(tabTroncons[0]), depart, arrivee, Integer.parseInt(tabTroncons[3]));
+      if(!mapTroncons.containsKey(depart)){
+        mapTroncons.put(depart,new HashSet<>());
+      }
+      mapTroncons.get(depart).add(arrivee);
+
     }
 
   }
 
 
   public void calculerCheminMinimisantNombreTroncons(String depart, String arrivee) throws IOException {
-    for (Troncon t: listeTroncons) {
-      System.out.println(t.toString());
-    }
+      LinkedList<Station> file = new LinkedList<>();
+      Set<Station> stationsVisitee = new HashSet<>();
+      Map<Station, Station> troncons = new HashMap<>();
+      Station stationDepart = new Station(depart);
+      Station stationArrivee = new Station(arrivee);
+
+      file.addFirst(stationDepart);
+      stationsVisitee.add(stationDepart);
+
+
+      file.removeLast();
+      for (Station s : mapTroncons.get(stationDepart)) {
+        if(!stationsVisitee.contains(s)) {
+          file.addFirst(s);
+          stationsVisitee.add(s);
+          troncons.put(s,stationDepart);
+          if(s == stationArrivee)
+        }
+      }
+
 
   }
 
